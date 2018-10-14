f5a11381c01907c80ba2a8b450c5af376b8d0c4c
doctordrau <doctordrau@yahoo.com>
2018-10-14 18:38:57 +0300

    Update player.py

--- mps_youtube/player.py
+++ mps_youtube/player.py
@@ -356,7 +356,7 @@ def stream_details(song, failcount=0, override=False, softrepeat=False):
         if not stream:
             raise IOError("No streams available")
             
-        if Config.MPV_USE_DASH.get and config.SHOW_VIDEO.get:
+        if Config.MPV_USE_DASH.get and Config.SHOW_VIDEO.get:
             list_update("--ytdl-format=bestvideo+bestaudio", args)
             url = "https://www.youtube.com/watch?v=" + song.ytid
             return [Config.PLAYER.get] + args + [url], songdata
